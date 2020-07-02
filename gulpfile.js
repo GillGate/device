8fecf4fca2cc2cd5ed4d24a83dd4211711157f9f
GillGate <gillgate@yandex.ru>
2020-07-02 15:36:50 +0300

    upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
 const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const gcmq = require('gulp-group-css-media-queries');
 const less = require('gulp-less');
-const browserSync = require('browser-sync').create();
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
-const del = require('del');
-const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
+const gcmq = require('gulp-group-css-media-queries');
+const plumber = require("gulp-plumber");
+const imagemin = require('gulp-imagemin');
+const imageminWebp = require('imagemin-webp');
+const svgstore = require("gulp-svgstore");
+const cheerio = require('gulp-cheerio');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const gulpif = require('gulp-if');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
-var config = {
+const isDev = (process.argv.indexOf('--dev') !== -1);
+const isProd = !isDev;
+const isSync = (process.argv.indexOf('--sync') !== -1);
+
+let config = {
     src: './src',
     build: './build',
     html: {
@@ -23,11 +34,11 @@ var config = {
         dest: '/fonts/'
     },
     js: {
-        src: '/js/*',
+        src: '/js/*.js',
         dest: '/js/'
     },
     img: {
-        src: '/img/*',
+        src: '/img/**/*',
         dest: '/img/'
     },
     css: {
@@ -41,97 +52,108 @@ var config = {
     }
 };
 
-gulp.task('html', function(){
-    gulp.src(config.src + config.html.src)
+function html() {
+    return gulp.src(config.src + config.html.src)
+        .pipe(posthtml([
+            include()
+        ]))
         .pipe(gulp.dest(config.build + config.html.dest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
+        .pipe(gulpif(isSync, browserSync.stream()));
+}
 
-gulp.task('fonts', function(){
-    gulp.src(config.src + config.fonts.src)
-        .pipe(gulp.dest(config.build + config.fonts.dest));
-});
-
-gulp.task('img', function(){
-    gulp.src(config.src + config.img.src)
-        .pipe(imagemin({
-            progressive: true,
-            optimizationLevel: 7
-        }))
-        .pipe(gulp.dest(config.build + config.img.dest));
-});
-
-gulp.task('js', function(){
-    gulp.src(config.src + config.js.src)
-      /*  .pipe(sourcemaps.init())
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))*/
-        .pipe(gulp.dest(config.build + config.js.dest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-gulp.task('css', function(){
-    gulp.src(config.src + config.css.src)
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(config.build + config.css.dest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-gulp.task('del', function(){
-    let path = config.build + '/*';
-    
-    if(path.substr(0, 1) === '/'){
-        console.log("never delete files from root :)");
-    }
-    else{
-        del.sync(path);
-    }
-});
-    
-gulp.task('all', ['del', 'html', 'fonts', 'img', 'js', 'css', 'less'], function(){
-
-});
-
-gulp.task('less', function(){
-   gulp.src(config.src + config.less.src)
+function styles() {
+    return gulp.src(config.src + config.less.src)
        .pipe(less())
+       .pipe(gulpif(isDev, sourcemaps.init()))
        .pipe(gcmq())
-       .pipe(sourcemaps.init())
        .pipe(autoprefixer({
-            browsers: ['> 0.01%'],
+            overrideBrowserslist: ['defaults'],
             cascade: false
        }))
+       .pipe(gulpif(isDev, gulp.dest(config.build + config.less.dest)))
        .pipe(cleanCSS({
-           level: 2
-       }))
-       .pipe(sourcemaps.write('.'))
+            level: 2
+        }))
+       .pipe(rename("style.min.css"))
+       .pipe(gulpif(isDev, sourcemaps.write('.')))
        .pipe(gulp.dest(config.build + config.less.dest))
-       .pipe(browserSync.reload({
-            stream: true
-        }));
-});
+       .pipe(gulpif(isSync, browserSync.stream()));
+}
 
-gulp.task('browserSync', function(){
-   browserSync.init({
-        server: {
-            baseDir: config.build
-        }
-    })
-});
+function grid(done) {
+	delete require.cache[require.resolve('./smartgrid.js')];
+	let settings = require('./smartgrid.js');
+	smartgrid('./src/less', settings);
+	done();
+}
 
-gulp.task('watch', ['browserSync'], function(){
-    gulp.watch(config.src + config.less.watch, ['less']);
-    gulp.watch(config.src + config.html.src, ['html']);
-    gulp.watch(config.src + config.js.src, ['js']);
-});
+function img() {
+    return gulp.src(config.src + config.img.src)
+        /*.pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.svgo()
+        ]))*/
+        .pipe(gulp.dest(config.build + config.img.dest));
+}
+
+function js(done) {
+    /*
+    return gulp.src(config.src + config.js.src)
+        .pipe(gulpif(isDev, sourcemaps.init()))
+        .pipe(concat('all.js'))
+        .pipe(gulpif(isDev, sourcemaps.write('.')))
+        .pipe(gulp.dest(config.build + config.js.dest))
+        .pipe(gulpif(isSync, browserSync.stream()));
+    */
+        done();
+}
+
+function fonts() {
+    return gulp.src(config.src + config.fonts.src)
+        .pipe(gulp.dest(config.build + config.fonts.dest));
+}
+
+function sprite() {
+    return gulp.src(config.src + config.img.src  + 'icon-*.svg')
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("sprite.svg"))
+        .pipe(gulp.dest(config.build + config.img.dest));
+}
+
+function clean() {
+    return del(['build/*']);
+}
+
+function watch() {
+    if (isSync) {
+        browserSync.init({
+            server: {
+                baseDir: config.build
+            }
+        });
+    }
+
+    gulp.watch(config.src + config.html.src, html);
+    gulp.watch(config.src + config.less.watch, styles);
+    gulp.watch(config.src + config.img.src, img);
+    gulp.watch(config.src + config.js.src, js);
+    gulp.watch('./smartgrid.js', grid);
+}
+
+const build = gulp.series(clean, 
+    gulp.parallel(html, styles, img, js, fonts, sprite));
+const all = gulp.series(build, watch);
+
+gulp.task('grid', grid);
+gulp.task('build', build);
+gulp.task('default', all);
